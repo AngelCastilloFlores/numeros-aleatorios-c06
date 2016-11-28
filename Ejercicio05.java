cce7b1d6155ce731e2c160e3ff69a1cd90ece9d3
Ángel Castillo Flores <akmusic74@gmail.com>
2016-11-28 19:15:07 +0100

    Add files via upload

---
+++ Ejercicio05.java
@@ -0,0 +1,33 @@
+import java.util.Scanner; // NO TERMINADO
+
+//Muestra 50 números enteros aleatorios entre 100 y 199 (ambos incluidos) separados por espacios. 
+//Muestra también el máximo, el mínimo y la media de esos números.
+
+public class Ejercicio05 {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		
+		int maximo = 100;
+		int minimo = 199;
+		int media;
+		
+		for (int i = 0; i <= 50; i++){
+			
+		int numero = (int)(Math.random()*100 + 100);
+		
+		System.out.print(" " + numero);
+		
+		
+		
+		
+		
+		}
+		
+		
+		
+		
+
+	}
+
+}
